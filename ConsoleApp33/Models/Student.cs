900cd500ef2aae14d12e6f765c9d5109fc27463e
Orxan <ibrahimzada003@gmail.com>
2023-11-26 01:34:26 +0400

    Add project files.

---
+++ ConsoleApp33/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp33.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }    
+        public int Point{ get; set; }
+
+        private static int Idp = 1;
+
+        public Student(string fullname,int point) 
+        { 
+            Id = Idp++;
+            Fullname = fullname;
+            Point = point;          
+        }
+        public void StudentInfo() 
+        {
+            Console.WriteLine($"Id:{Id}");
+            Console.WriteLine($"Fullname:{Fullname}");
+            Console.WriteLine($"Point:{Point}");
+        }
+
+    }
+
+}
---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp33.Models;
+
+Student student1 = new("Fuad", 93);
+Student student2 = new("Orxan", 85);
+
+student1.StudentInfo();
+student2.StudentInfo();
